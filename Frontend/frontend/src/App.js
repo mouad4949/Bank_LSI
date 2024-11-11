94afae97b8902df2be946f3789a4df08c785819a
mouad4949 <mouadrguibi900@gmail.com>
2024-11-11 13:20:25 +0100

    Sauvegarde des modifications locales

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import ListCompte from './pages/ListCompte'
+import Navbar from './Components/Navbar';
+import { Routes , Route } from 'react-router-dom';
+import Clients from './pages/Clients';
+import Comptes from './pages/Comptes';
+import Employes from './pages/Employes';
+import Operations from './pages/Operations';
+import Groups from './pages/Groups';
 
 function App() {
   return (
    <>
-    <ListCompte/>
+   <Navbar/>
+    <Routes>
+      <Route path='/clients' element={<Clients/>}/>
+      <Route path='/compts' element={<Comptes/>}/>
+      <Route path='/employes' element={<Employes/>}/>
+      <Route path='/operations' element={<Operations/>}/>
+      <Route path='/groups' element={<Groups/>}/>
+
+
+    </Routes>
    </>
   );
 }
